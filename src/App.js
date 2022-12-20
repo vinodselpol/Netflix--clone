c8fe7cc3e6968b907566cc3b86196b177b67c67b
vinodselpol <vinodselpol@gmail.com>
2022-12-20 14:01:27 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomeScreen from './HomeScreen';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      
+      <HomeScreen />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD628TCwLOTZkFRd1awe0Qn44xBzW6qrxA",
+    authDomain: "netflix-clone-3c593.firebaseapp.com",
+    projectId: "netflix-clone-3c593",
+    storageBucket: "netflix-clone-3c593.appspot.com",
+    messagingSenderId: "236369037420",
+    appId: "1:236369037420:web:0cc7f33838613699e47049",
+    measurementId: "G-2R28RY7B0V"
+  };
