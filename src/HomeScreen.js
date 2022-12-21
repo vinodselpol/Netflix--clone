1a644f69061780758516cc5a8a8d15d221cac82b
vinodselpol <vinodselpol@gmail.com>
2022-12-20 19:43:01 -0600

    row update

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react'
 import "./HomeScreen.css"
 import Nav from './Nav'
 import Banner from './Banner'
+import requests from './Requests'
+import Row from "./Row"
 function HomeScreen() {
   return (
     <div className='homeScreen'>
@@ -11,11 +13,55 @@ function HomeScreen() {
 
         <Banner />
 
+
+        <Row 
+        title ="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow/>
+
+        <Row 
+        title ="TRENDING NOW"
+        fetchUrl={requests.fetchTrending}
+        />
+
+        <Row 
+        title ="TOP RATED"
+        fetchUrl={requests.fetchTopRated}
+        />
+
+        <Row 
+        title ="ACTION MOVIES"
+        fetchUrl={requests.fetchActionMovies}
+        />
+
+
+        <Row 
+        title ="COMEDY MOVIES"
+        fetchUrl={requests.fetchComedyMovies}
+        />
+
+        <Row 
+        title ="HORROR MOVIES"
+        fetchUrl={requests.fetchHorrorMovies}
+        />
+
+        <Row 
+        title ="ROMANCE MOVIES"
+        fetchUrl={requests.fetchRomanceMovies}
+        />
+
+        <Row 
+        title ="DOCUMENTARIES"
+        fetchUrl={requests.fetchDocumentaries}
+        />
+
+
+
       
 
         
 
-        {/* Row */}
+        
     </div>
   )
 }
