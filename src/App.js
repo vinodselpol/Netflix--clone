1f79be6da7d17db5e4fc314e5e9dad38366baaf7
vinodselpol <vinodselpol@gmail.com>
2022-12-21 18:04:54 -0600

    LoginScreen added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
+      <Router>
+
+        {!user ? (
+          <LoginScreen />
+        ) :(
+          <Routes>
+         
+          <Route exact path ="/"
+          element = {<HomeScreen />} >
+          </Route>
+          <Route exact path ="/test"
+          element = {<h1>This is the test</h1>} >
+          </Route>
+        </Routes>
+
+        )}
+        
+      </Router>
       
-      <HomeScreen />
     </div>
   );
 }
